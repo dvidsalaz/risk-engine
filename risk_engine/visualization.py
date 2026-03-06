db5f1a4982cfffc14779ca6c7ec97a910de11b64
David S <dvidsalaz@gmail.com>
2026-03-05 18:02:41 -0600

    moved visualization logic from notebook  into visualization.py

--- risk_engine/visualization.py
+++ risk_engine/visualization.py
@@ -1,2 +1,13 @@
+import matplotlib.pyplot as plt
 
 
+def visualization(returns, var, cvar):
+    plt.hist(returns)
+    plt.axvline(var, label='VaR')
+    plt.axvline(cvar, linestyle='dashed', label='cVaR')
+    plt.axvspan(xmin=returns.min().min(), xmax=var, color='red', alpha=0.3)
+    plt.title('Historical VaR and cVaR (ES)')
+    plt.xlabel('Returns')
+    plt.ylabel('Frequency')
+
+    return plt.legend(), plt.show()
